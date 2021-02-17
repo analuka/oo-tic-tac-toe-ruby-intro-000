f5417e3d217c70cd1591e8a7918d9641c5f8c319
Anna <ajlukasik@gmail.com>
2021-02-17 10:07:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,16 +2,19 @@ class TicTacToe
   # def initialize (board = nil)
   #   @board = board || Array.new (9, " ")
   # end
-  attr_accessor :board
+  # attr_accessor :board
+  # def initialize
+  #   # @board = Array.new (9, " ")
+  #   # @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  #   set_board
+  # end
+  #
+  # def set_board
+  #   @board = Array.new (9, " ")
+  # end
+
   def initialize
-    # @board = Array.new (9, " ")
-    # @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    set_board
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
   end
-
-  def set_board
-    @board = Array.new (9, " ")
-  end
-
-
 end
