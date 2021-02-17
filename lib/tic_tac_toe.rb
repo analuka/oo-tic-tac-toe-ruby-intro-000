4c9e26c0eb3337799641cb4e47ac80bae9e5ff71
Anna <ajlukasik@gmail.com>
2021-02-17 10:01:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,8 +4,8 @@ class TicTacToe
   # end
 
   def initialize
-    # board = 
-    @board = Array.new (9, " ")
+    board = Array.new (9, " ")
+    @board = board
     # @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
 
